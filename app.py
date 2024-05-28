19f8a228ccec601008d2276916becedf0cd79766
ferhanmm <ferhanmm@gmail.com>
2024-05-28 16:11:05 -0500

    Update app.py
    full header set for debug

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def index():
             ip_addr = "99.61.181.42" 
 
         locationcheck = iplookup(ip_addr)
+        print(f"request.environ: {request.environ}")
         print(f"HTTP_X_FORWARDED_FOR: {request.environ.get('HTTP_X_FORWARDED_FOR')}")
         print(f"REMOTE_ADDR: {request.environ['REMOTE_ADDR']}")
         print(f"{ip_addr}: {locationcheck}")
